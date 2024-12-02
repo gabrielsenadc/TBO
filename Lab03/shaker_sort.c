197a3778bb3c163ef76895d885f725d5d8cdeb2b
gabrielsenadc <gabriel.cunha.74@edu.ufes.br>
2024-12-02 20:35:21 -0300

    Terminando Lab03/*.c  e começando a coletar dados

---
+++ Lab03/shaker_sort.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "item.h"
+
+void sort(Item *a, int lo, int hi){
+    int swaped = 1;
+    while(swaped){
+        swaped = 0;
+        for(int i = 0, j = hi - 1; i <= j; i++, j--){
+        if(less(a[i + 1], a[i])) swaped = 1;
+        compexch(a[i], a[i + 1]);
+
+        if(less(a[j], a[j - 1])) swaped = 1;
+        compexch(a[j - 1], a[j]);
+        }
+    }
+    
+}

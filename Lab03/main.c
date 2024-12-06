4f1b14edacc9a07c5667ac37faff3ea4287dd826
gabrielsenadc <gabriel.sena.cunha@gmail.com>
2024-12-06 09:18:15 -0300

    Coletando dados Lab03

--- Lab03/main.c
+++ Lab03/main.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 #include "item.h"
 #include <time.h>
+#include <string.h>
 
 extern void sort(Item *a, int lo, int hi);
 
 double read_sort_print(char * path, int size){
+    printf("%d\n", size);
     FILE * file = fopen(path, "r");
     Item * vet = calloc(size, sizeof(int));
     for(int i = 0; i < size; i++) fscanf(file, "%d", &vet[i]);
@@ -21,8 +23,22 @@ double read_sort_print(char * path, int size){
     return (( double ) end - (double) start ) / CLOCKS_PER_SEC ;
 }
 
-int main(){
-    double time = read_sort_print("in/unif_rand/1Kunif_rand.txt", 1000);
+int main(int argc, char *argv[]){
+    if(argc <= 1){
+        printf("Sem parametros!\n");
+        return 1;
+    }
+
+    char path[100];
+    char * option = "unif_rand";
+    sprintf(path, "in/%s/%s%s.txt", option, argv[1], option);
+
+    int n = 0;
+    if(!strcmp(argv[1], "1K")) n = 1000;
+    if(!strcmp(argv[1], "10K")) n = 10000;
+    if(!strcmp(argv[1], "100K")) n = 100000;
+
+    double time = read_sort_print(path, n);
 
     printf("\ntime: %lf\n",time);
     return 0;
